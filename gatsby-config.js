0547b221936b80272f99122ab53931423ceeffb9
Hassain Saeed <hassain.saeed@gmail.com>
2021-12-14 00:46:41 -0800

    Added eslint

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,61 +1,35 @@
-require(`dotenv`).config()
-
-const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
-
 module.exports = {
-  siteMetadata: {
-    // You can overwrite values here that are used for the SEO component
-    // You can also add new values here to query them like usual
-    // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Iris and Ahmad`,
-    siteTitleAlt: `Iris and Ahmad Wedding Website`,
-    siteHeadline: `Iris and Ahmad Wedding Website`,
-    siteUrl: `https://irisandahmad.com`,
-    siteDescription: `Wedding website for Iris and Ahmad`,
-    siteLanguage: `en`,
-    siteImage: `/banner.jpg`,
-    author: `@hassainsaeed`,
-  },
   plugins: [
+    'gatsby-plugin-sass',
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-offline',
     {
-      resolve: `@lekoarts/gatsby-theme-cara`,
-      // See the theme's README for all available options
-      options: {},
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `IrisAndAhmad`,
-        description: `Wedding website for Iris and Ahmad`,
-        start_url: `/`,
-        background_color: `#141821`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#f6ad55`,
-        display: `standalone`,
-        icons: [
-          {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
+        name: 'images',
+        path: `${__dirname}/src/images/`,
       },
     },
-    `gatsby-plugin-gatsby-cloud`,
-    shouldAnalyseBundle && {
-      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+    {
+      resolve: 'gatsby-source-filesystem',
       options: {
-        analyzerMode: `static`,
-        reportFilename: `_bundle.html`,
-        openAnalyzer: false,
+        name: 'pages',
+        path: `${__dirname}/src/pages/`,
       },
     },
-  ].filter(Boolean),
-}
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Gatsby Simplefolio',
+        short_name: 'Simplefolio',
+        start_url: '/',
+        background_color: '#fff',
+        theme_color: '#02aab0',
+        display: 'standalone',
+        icon: 'src/images/favicon.png',
+      },
+    },
+  ],
+};
